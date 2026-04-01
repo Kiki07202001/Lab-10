d84bb07cb861e709b4a9a99ddd80c70a2f2ef4dd
Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
2026-03-31 21:40:10 -0400

    getProductByName method added

--- node.js
+++ node.js
@@ -88,4 +88,31 @@ Total 4 (delta 2), reused 0 (delta 0), pack-reused 0
 remote: Resolving deltas:   0% (0/2)[Kremote: Resolving deltas:  50% (1/2)[Kremote: Resolving deltas: 100% (2/2)[Kremote: Resolving deltas: 100% (2/2), completed with 2 local objects.[K
 To https://github.com/Kiki07202001/Lab-10.git
    cf62680..ce9b329  main -> main
+[1m[7m%[27m[1m[0m                                                                                                            [0m[27m[24m[Jkikibrusey@Kikis-MacBook-Air Lab-10 % [K[?2004hggit add .[?2004l
+[1m[7m%[27m[1m[0m                                                                                                            [0m[27m[24m[Jkikibrusey@Kikis-MacBook-Air Lab-10 % [K[?2004hggit commit -m "Add getInventoryValue from    method to Store class"ed  [?2004l
+[main 47444ca] Add getInventoryValue method to Store class
+ Committer: Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
+Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly. Run the
+following command and follow the instructions in your editor to edit
+your configuration file:
+
+    git config --global --edit
+
+After doing this, you may fix the identity used for this commit with:
+
+    git commit --amend --reset-author
+
+ 2 files changed, 39 insertions(+), 1 deletion(-)
+[1m[7m%[27m[1m[0m                                                                                                            [0m[27m[24m[Jkikibrusey@Kikis-MacBook-Air Lab-10 % [K[?2004hggit push origin main[?2004l
+Enumerating objects: 7, done.
+Counting objects:  14% (1/7)Counting objects:  28% (2/7)Counting objects:  42% (3/7)Counting objects:  57% (4/7)Counting objects:  71% (5/7)Counting objects:  85% (6/7)Counting objects: 100% (7/7)Counting objects: 100% (7/7), done.
+Delta compression using up to 8 threads
+Compressing objects:  25% (1/4)Compressing objects:  50% (2/4)Compressing objects:  75% (3/4)Compressing objects: 100% (4/4)Compressing objects: 100% (4/4), done.
+Writing objects:  25% (1/4)Writing objects:  50% (2/4)Writing objects:  75% (3/4)Writing objects: 100% (4/4)Writing objects: 100% (4/4), 737 bytes | 737.00 KiB/s, done.
+Total 4 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas:   0% (0/2)[Kremote: Resolving deltas:  50% (1/2)[Kremote: Resolving deltas: 100% (2/2)[Kremote: Resolving deltas: 100% (2/2), completed with 2 local objects.[K
+To https://github.com/Kiki07202001/Lab-10.git
+   ce9b329..47444ca  main -> main
 [1m[7m%[27m[1m[0m                                                                                                            [0m[27m[24m[Jkikibrusey@Kikis-MacBook-Air Lab-10 % [K[?2004h
